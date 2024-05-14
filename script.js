2fe347b0f3b82db7d7163daaba6760c78dcf2d46
ImShad0w <kirill19042006@gmail.com>
2024-05-14 23:58:24 +0200

    Created button and grid resizing logic

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const container = document.getElementById("container");
+const resize = document.getElementById("resize")
+let size;
 
-for (let i = 0; i < 16; i++){
-  const row = document.createElement("div");
-  row.className = "row";
-  container.appendChild(row);
+function createGrid(rows, cols){
+  for (let i = 0; i < (rows * cols); i++){
+    container.style.setProperty("--grid-rows", rows);
+    container.style.setProperty("--grid-cols", cols);
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare")
+    container.appendChild(gridSquare);
+    gridSquare.addEventListener("mouseover", (event) =>{
+    gridSquare.style.cssText += "background-color: black;"
+  });
+  };
+};
 
-  for (let i = 0; i < 17; i++){
-    const gridItem = document.createElement("div");
-    gridItem.className = "grid-item"
-    row.appendChild(gridItem)
-    gridItem.addEventListener("mouseover", (event) =>{
-      gridItem.style.background = "black"
-    })
+resize.addEventListener("click", function changeSize(size){
+  size = parseInt(prompt("Type number between 0, 100"))
+  container.innerHTML = "";
+  if(size < 100){
+    createGrid(size, size);
   }
-}
+  else{
+    alert("That number is not valid!")
+    createGrid(16,16)
+  };
+});
 
-
-
+createGrid(16, 16);
